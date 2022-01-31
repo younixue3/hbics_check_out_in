02dd09172e81cbd3b8288bcf137bdcbc15d3ea43
younixue3 <bussiness.needs.rctka@gmail.com>
2022-01-31 16:02:18 +0800

    Fixing ListStaff

--- lib/card/CardApproval.dart
+++ lib/card/CardApproval.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import '../component/ListStaff.dart';
 
 class CardApproval extends StatefulWidget {
   const CardApproval({Key? key}) : super(key: key);
@@ -23,13 +24,14 @@ class _CardApprovalState extends State<CardApproval> {
               borderRadius: BorderRadius.circular(15),
             ),
             child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
                 width: 500,
-                child: Row(
-                  children: [
-                    // Icon(Icons.search),
-                    TextField(
-                        decoration: InputDecoration(border: InputBorder.none)),
-                  ],
+                child: TextField(
+                  style: TextStyle(fontSize: 15),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Search...',
+                  ),
                 )),
           ),
           Card(
@@ -42,186 +44,21 @@ class _CardApprovalState extends State<CardApproval> {
               padding: EdgeInsetsDirectional.all(10),
               child: Column(
                 children: [
-                  TextButton(
-                    style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.all(
-                            Color.fromARGB(50, 73, 114, 209)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15)))),
-                    onPressed: () {},
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(80),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                        'assets/public/57774948_2440392989306240_790466375524024320_n.jpg'))),
-                          ),
-                          Container(
-                            width: 220,
-                            margin: EdgeInsets.only(left: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Ricko Tiaka',
-                                  style: TextStyle(
-                                      fontSize: 18, color: Colors.black),
-                                ),
-                                Text(
-                                  'Web Developer',
-                                  style: TextStyle(
-                                      fontSize: 14, color: Colors.black),
-                                )
-                              ],
-                            ),
-                          ),
-                          Container(
-                            width: 40,
-                            margin: EdgeInsets.only(left: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(Icons.check_circle),
-                                  color: Colors.green,
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.all(
-                            Color.fromARGB(50, 73, 114, 209)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15)))),
-                    onPressed: () {},
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(80),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                        'assets/public/57774948_2440392989306240_790466375524024320_n.jpg'))),
-                          ),
-                          Container(
-                            width: 220,
-                            margin: EdgeInsets.only(left: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Ricko Tiaka',
-                                  style: TextStyle(
-                                      fontSize: 18, color: Colors.black),
-                                ),
-                                Text(
-                                  'Web Developer',
-                                  style: TextStyle(
-                                      fontSize: 14, color: Colors.black),
-                                )
-                              ],
-                            ),
-                          ),
-                          Container(
-                            width: 40,
-                            margin: EdgeInsets.only(left: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(Icons.check_circle),
-                                  color: Colors.green,
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  TextButton(
-                    style: ButtonStyle(
-                        overlayColor: MaterialStateProperty.all(
-                            Color.fromARGB(50, 73, 114, 209)),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15)))),
-                    onPressed: () {},
-                    child: Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(80),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                        'assets/public/57774948_2440392989306240_790466375524024320_n.jpg'))),
-                          ),
-                          Container(
-                            width: 220,
-                            margin: EdgeInsets.only(left: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Ricko Tiaka',
-                                  style: TextStyle(
-                                      fontSize: 18, color: Colors.black),
-                                ),
-                                Text(
-                                  'Web Developer',
-                                  style: TextStyle(
-                                      fontSize: 14, color: Colors.black),
-                                )
-                              ],
-                            ),
-                          ),
-                          Container(
-                            width: 40,
-                            margin: EdgeInsets.only(left: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: Icon(Icons.check_circle),
-                                  color: Colors.green,
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
+                  ListStaff(
+                      '57774948_2440392989306240_790466375524024320_n.jpg',
+                      'Ricko Tianaka',
+                      'Web Developer',
+                      true),
+                  ListStaff(
+                      '57774948_2440392989306240_790466375524024320_n.jpg',
+                      'Rikhi Angry',
+                      'Graphic Designer',
+                      false),
+                  ListStaff(
+                      '57774948_2440392989306240_790466375524024320_n.jpg',
+                      'Ryan Dzul',
+                      'Video Editor',
+                      true),
                 ],
               ),
             ),
---
+++ lib/component/ListStaff.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class ListStaff extends StatefulWidget {
+  // const ListStaff({Key? key}) : super(key: key);
+  final String image;
+  final String name;
+  final String position;
+  final bool status;
+
+  ListStaff({@required this.image});
+  @override
+  _ListStaffState createState() => _ListStaffState();
+  // _ListStaff(this.image, this.name, this.position, this.status);
+}
+
+class _ListStaffState extends State<ListStaff> {
+  late String image;
+  late String name;
+  late String position;
+  late bool status;
+
+  // ListStaff(this.image, this.name, this.position, this.status);
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+          overlayColor:
+              MaterialStateProperty.all(Color.fromARGB(50, 73, 114, 209)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))),
+      onPressed: () {
+        this.status = false;
+      },
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(80),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage('assets/public/' + image))),
+            ),
+            Container(
+              width: 220,
+              margin: EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                  Text(
+                    position,
+                    style: TextStyle(fontSize: 14, color: Colors.black),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 40,
+              margin: EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Icon(
+                    Icons.check_circle,
+                    color: Colors.green,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+// class ListStaff extends StatelessWidget {
+//   // const ListStaff({Key? key, title, image}) : super(key: key);
+//   final String image;
+//   final String name;
+//   final String position;
+//   late bool status;
+//   ListStaff(this.image, this.name, this.position, this.status);
+
+//   @override
+//   Widget build(BuildContext context) {}
+// }
